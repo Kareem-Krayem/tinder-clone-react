1a7c0dc985cf946317c019918524f92cf1c80378
Kareem Krayem <50558667+Kareem-Krayem@users.noreply.github.com>
2020-09-13 19:37:18 +0300

    Update firebase.js

--- tinder-clone/src/firebase.js
+++ tinder-clone/src/firebase.js
@@ -1,13 +1,7 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyABxPpojYhnmIyXk9mG0QdoEXv2UxwpedA",
-    authDomain: "kareem-tinder-clone.firebaseapp.com",
-    databaseURL: "https://kareem-tinder-clone.firebaseio.com",
-    projectId: "kareem-tinder-clone",
-    storageBucket: "kareem-tinder-clone.appspot.com",
-    messagingSenderId: "885251383809",
-    appId: "1:885251383809:web:0e441cb2fff1fbafb99600"
+//here are the configurations that you need to put from firebase
 };
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const database = firebaseApp.firestore();
